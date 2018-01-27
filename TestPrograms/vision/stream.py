39c17da9790e668224b192d340c7aa2fab10c95e
Unknown <jon.bramsch@okstate.edu>
2018-01-26 23:47:17 -0600

    File organization

--- TestPrograms/vision/stream.py
+++ TestPrograms/vision/stream.py
@@ -5,7 +5,7 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    frame = cap.read()
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
