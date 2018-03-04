bd4b197a59da4326b73e2a7348bad6b9285be18e
betho214 <bschaf8@gmail.com>
2018-03-03 18:59:24 -0600

    a

--- src/test_robot_server_stream.py
+++ src/test_robot_server_stream.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 import pickle
 import socket
 import Adafruit_PCA9685 as pca
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+
 
 pwm = pca.PCA9685()
 pwm.set_pwm_freq(60)
@@ -20,28 +22,15 @@ s.listen(1)
 print("waiting for connection...")
 conn, addr = s.accept()
 # print("connected to: ", addr)
-
-# print("connected to: ", addr)
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
     pickled_data = conn.recv(1024)
     if not pickled_data:
         break
     data = pickle.loads(pickled_data)
-    FL_motor = ((-data[0][1] + data[0][0]) * -1 + 400)
-    FR_motor = ((-data[0][1] - data[0][0]) * -1 + 400)
-    BL_motor = ((-data[0][1] - data[0][0]) * -1 + 400)
-    BR_motor = ((-data[0][1] + data[0][0]) * -1 + 400)
+    FL_motor = ((-data[0][1] + data[0][0]) * -2 + 420)
+    FR_motor = ((-data[0][1] - data[0][0]) * 2 + 420)
+    BL_motor = ((-data[0][1] - data[0][0]) * -2 + 420)
+    BR_motor = ((-data[0][1] + data[0][0]) * 2 + 420)
     print("FL:", FL_motor, " FR:", FR_motor, " BL:", BL_motor, " BR:", BR_motor)
 
     pwm.set_pwm(FL_channel, 0, FL_motor)
