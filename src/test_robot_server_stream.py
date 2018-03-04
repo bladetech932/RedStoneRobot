9efa56e61f6c81680fb2f6f19a59b4fa4fd0ef92
betho214 <bschaf8@gmail.com>
2018-03-03 18:44:57 -0600

    i hope it works

--- src/test_robot_server_stream.py
+++ src/test_robot_server_stream.py
@@ -23,24 +23,18 @@ conn, addr = s.accept()
 
 # print("connected to: ", addr)
 while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 
-        # Capture frame-by-frame
-        ret, frame = cap.read()
 
-        # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Display the resulting frame
-        cv2.imshow('frame', gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
-
-    
     pickled_data = conn.recv(1024)
     if not pickled_data:
         break
