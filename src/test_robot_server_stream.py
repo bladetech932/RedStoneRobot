36339773251530317103ae16ec49f32be3ea6304
betho214 <bschaf8@gmail.com>
2018-03-03 20:07:59 -0600

    fix

--- src/test_robot_server_stream.py
+++ src/test_robot_server_stream.py
@@ -9,6 +9,7 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+
 def videos():
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -57,15 +58,6 @@ while True:
 
     videos():
 
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    if data[1][0] is 1:
-        cv2.imshow('frame', gray)
 
 
 # When everything done, release the capture
