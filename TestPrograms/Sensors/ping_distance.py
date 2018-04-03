83f1f7e1507e064614a0901e82b43235fb3e3324
bladetech932 <jon.bramsch@okstate.edu>
2018-04-03 17:41:39 -0500

    remove cad

--- TestPrograms/Sensors/ping_distance.py
+++ TestPrograms/Sensors/ping_distance.py
@@ -7,39 +7,39 @@ echo = 24
 
 print("distance measurement in progress")
 
-GPIO.setup(trig,GPIO.OUT)
-GPIO.setup(echo,GPIO.IN)
+GPIO.setup(trig, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
 
 
 count = 0
 total = 0
 avg = 0
 
-GPIO.output(trig,False)
+GPIO.output(trig, False)
 print("Settle")
 time.sleep(2)
 while True:
-        #count += 1
-        GPIO.output(trig, True)
-        time.sleep(0.00001)
-        GPIO.output(trig, False)
+    #count += 1
+    GPIO.output(trig, True)
+    time.sleep(0.00001)
+    GPIO.output(trig, False)
 
-        while GPIO.input(echo)==0:
-                #print("time is ",time.time())
-                pulse_start = time.time()
-        while GPIO.input(echo)==1:
-                #print("time is ",time.time())
-                pulse_end = time.time()
-        pulse_length = pulse_end - pulse_start
+    while GPIO.input(echo) == 0:
+        #print("time is ",time.time())
+        pulse_start = time.time()
+    while GPIO.input(echo) == 1:
+        #print("time is ",time.time())
+        pulse_end = time.time()
+    pulse_length = pulse_end - pulse_start
 
-        dist = 17150 * pulse_length
-        #total += dist
-        #avg = total / count
-        #if count %10 == 0:
-        if dist <= 10000:
-                print("distance:",round(dist),"cm")
-                #count = 0
-                #total = 0
-        time.sleep(.02)
+    dist = 17150 * pulse_length
+    #total += dist
+    #avg = total / count
+    # if count %10 == 0:
+    if dist <= 10000:
+        print("distance:", round(dist), "cm")
+        #count = 0
+        #total = 0
+    time.sleep(.02)
 
 GPIO.cleanup()
